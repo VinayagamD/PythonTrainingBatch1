b4977bbe5a110cbe34458aeef507182a84767e8c
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-06 20:02:10 +0530

    Learned about number data type and type keyword

---
+++ basics/datatypes/datatype_number.py
@@ -0,0 +1,32 @@
+"""
+Here we are going practise 
+Number Data type
+"""
+
+# Learn about integer datatype
+print("Learning Int datatype")
+a = 10
+print("Type of a: ", type(a))
+
+print(a + 11)
+print(a)
+a = a + 11
+print(a)
+print(a - 5)
+
+# a = "String"
+# print(type(a))
+
+print("-----------------------------------------------")
+#  Float Data type
+print("Learning Float Datatype")
+b = 4.10
+print(b)
+print(type(b))
+
+print("-----------------------------------------------")
+#  Float Data type
+print("Learning Complex Datatype")
+c = 4 + 1j
+print(c)
+print(type(c))
