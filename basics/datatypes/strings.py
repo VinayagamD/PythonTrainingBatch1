457a305da17a452800fd51abdb7a8b4e7ff6fab1
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-13 19:52:26 +0530

    Learned String Operations and Input

--- basics/datatypes/strings.py
+++ basics/datatypes/strings.py
@@ -36,3 +36,50 @@ print(mystring[:5])
 
 mystring = mystring[:5]
 print(mystring)
+
+
+mystring = "programiz"
+extrated_string = mystring[5:-2]
+print(extrated_string)
+s = ('Hello' 'World')
+print(s)
+
+print('a' in mystring)
+print('at' not in 'battle')
+
+# enumurate
+mystring = 'cold'
+list_enumurate = list(enumerate(mystring))
+print(list_enumurate)
+
+mystring = 'i\'m vinay'
+print(mystring)
+print(mystring, '\n', mystring)
+print("This is \x48\x45\x58 representation")
+print(r"This is \x48\x45\x58 representation")
+
+default_order = "{}, {} and {}".format('John','Bill','Sean')
+print('\n--- Default Order ---')
+print(default_order)
+print('\n--- Positional Order ---')
+positional_order = "{1}, {0} and {2}".format('John','Bill','Sean')
+print(positional_order)
+print('\n--- Keyword Order ---')
+positional_order = "{s}, {b} and {j}".format(j='John',b='Bill',s='Sean')
+print(positional_order)
+
+# Binary
+print("Binary representation of {0} is {0:b}".format(12))
+print("Binary representation of {0} is {0:e}".format(156.33))
+print("Binary representation of {0} is {0:.3f}".format(2/3))
+print("|{:<10}|{:^10}|{:>10}|".format('butter','bread','ham'))
+
+x = 12.3456789
+print('The value of x is %3.2f' %x)
+print('The value of x is %3.4f' %x)
+
+print(' '.join(['This', 'will', 'join', 'all', 'words', 'into', 'a', 'string']))
+print('Happy New Year'.find('ew'))
+print('Happy New Year'.replace('Happy','Brilliant'))
+
+
---
+++ basics/inputs/intputs_frompython.py
@@ -0,0 +1,7 @@
+"""
+Learning to take from users
+"""
+
+data = input("Enter a value")
+print(data)
+print(type(data))
