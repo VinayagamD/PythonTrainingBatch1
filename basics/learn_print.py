fea846fb89ad6f66832a4940939e3cf5b1ff3289
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-24 11:22:50 +0530

    Pythonbatch2 trainig code commit

--- basics/learn_print.py
+++ basics/learn_print.py
@@ -7,20 +7,20 @@ This is generally used for explaining class and methods
 # Single Line comment genrally used for the explaining variables and TODO:
 # TODO : 
 # Normal Print
-print("Hi Joshua")
+print("Hi Midhul")
 
 # Print with end
 print("Hi Joshua", end='  ')
-print("Hi Joshua")
+# print("Hi Joshua")
 print("Hi Joshua", end='@')
 
 print()
 # Print with serparator
-print("hi Joshua", "Your Learning Python Class", "Using Teams")
-print("hi Joshua", "Your Learning Python Class", "Using Teams", sep=",")
-print("hi Joshua", "Your Learning Python Class", "Using Teams", sep="\t")
-print("hi Joshua", "Your Learning Python Class", "Using Teams", sep="\n")
+print("hi Midhul", "Your Learning Python Class", "Using Google Meets")
+print("hi Midhul", "Your Learning Python Class", "Using Teams", sep=",")
+print("hi Midhul", "Your Learning Python Class", "Using Teams", sep="\t")
+print("hi Midhul", "Your Learning Python Class", "Using Teams", sep="\n")
 
 # print file name
-printfile = open("file.txt", 'w+')
-print("hi Joshua", "Your Learning Python Class", "Using Teams", file=printfile)
+printfile = open("file.html", 'w+')
+print("hi Midhul", "Your Learning Python Class", "Using Teams", file=printfile)
