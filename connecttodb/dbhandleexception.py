bcffa747124ee2cce55c372abdb9d40821ce8159
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-18 19:15:25 +0530

    Transaction and rollback

---
+++ connecttodb/dbhandleexception.py
@@ -0,0 +1,23 @@
+import mysql.connector
+config = {"user": "vinay",
+            "password": 'drago',
+            "host": 'localhost',
+            "database": 'test_python_db'
+            }
+
+cnx = mysql.connector.connect(**config)
+
+try:
+
+    cursor = cnx.cursor()
+    query = "INSERT INTO studentss (name) VALUES (%s)"
+    val = ("vinay",)
+    cursor.execute(query, val)
+    cnx.commit()
+    print(cursor.rowcount, "record inserted.")
+
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
+    cnx.rollback()
+finally:
+    cnx.close()
